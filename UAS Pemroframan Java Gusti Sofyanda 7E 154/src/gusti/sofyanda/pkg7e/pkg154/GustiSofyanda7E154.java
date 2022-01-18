405f13f5f11011321f7e3d9154e11ecfbb087260
Gustisofyanda7 <83197649+Gustisofyanda7@users.noreply.github.com>
2022-01-18 08:12:25 +0700

    Add files via upload

---
+++ UAS Pemroframan Java Gusti Sofyanda 7E 154/src/gusti/sofyanda/pkg7e/pkg154/GustiSofyanda7E154.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gusti.sofyanda.pkg7e.pkg154;
+
+/**
+ *
+ * @author user
+ */
+public class GustiSofyanda7E154 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
